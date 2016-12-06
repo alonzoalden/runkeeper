62bc3e849bfc32a1d750ead206d667ca69a82975
Alonzo Alden <alonzoalden@gmail.com>
2016-12-06 15:25:25 -0800

    updating

--- client/app/addrun/addrun.js
+++ client/app/addrun/addrun.js
@@ -5,7 +5,11 @@ angular.module('runkeeper.addrun', [])
 
   $scope.run = {};
   $scope.run.exertion = 5;
-  $scope.tooltip = 'HELLLOOO';
+  $scope.tooltipTime = 'Enter the total amount of minutes you\'ve ran.';
+  $scope.tooltipNotes = 'Enter any notes or messages you may want to record.';
+  $scope.tooltipDistance = 'Enter your total distance ran in miles. Decimals are OK.';
+  $scope.tooltipExertion = 'This is your perceived level of exertion. 1 Being low and 10 being maximum. 1 would be the equivilent to a casual walk and ten would be similar to a race.';
+
   $scope.processRun = function () {
     $scope.loading = true;
     MyProfile.addOne($scope.run)
