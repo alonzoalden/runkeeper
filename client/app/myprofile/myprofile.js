1677ce4a1298e6cafad7927ba510dc19343f0469
Alonzo Alden <alonzoalden@gmail.com>
2016-12-06 13:56:45 -0800

    updating

--- client/app/myprofile/myprofile.js
+++ client/app/myprofile/myprofile.js
@@ -8,7 +8,7 @@ angular.module('runkeeper.myprofile', [])
     MyProfile.getAll()
       .then(function (runs) {
         $scope.data.runs = runs;
-
+  console.log($scope.data.runs.exertion)
       })
       .catch(function (error) {
         console.error(error);
